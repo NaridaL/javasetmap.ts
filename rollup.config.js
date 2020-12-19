224c0dfb912ce6cc8f9820e2e8fd09e158c766c5
Adrian Leonhard <adrianleonhard@gmail.com>
2020-12-19 17:22:57 +0100

    update project

--- rollup.config.js
+++ rollup.config.js
@@ -1,31 +1,28 @@
-import sourcemaps from "rollup-plugin-sourcemaps"
 import * as fs from "fs"
+import typescriptPlugin from "@rollup/plugin-typescript"
+import typescript from "typescript"
 
-const pkg = JSON.parse(fs.readFileSync("package.json"))
+const compress = false
+const pkg = JSON.parse(fs.readFileSync("package.json", "utf-8"))
 export default {
-  input: "out/index.js",
-  output: [
-    {
-      format: "cjs",
-      file: "dist/bundle.js",
+  input: "src/index.ts",
+  output: ["umd", "es"].map(format =>
+    ({
+      format,
+      entryFileNames: "[name].[format]" + (compress ? ".min" : "") + ".js",
       sourcemap: true,
-      sourcemapExcludeSources: true,
-    },
-    {
-      format: "es",
-      sourcemap: true,
-      file: "dist/bundle.module.js",
-      sourcemapExcludeSources: true,
-    },
-  ],
+      name: pkg.umdGlobal,
+      dir: "lib",
+      sourcemapExcludeSources: true
+    })),
   external: Object.keys(pkg.dependencies || {}),
-  plugins: [sourcemaps()],
-  onwarn: function (warning, warn) {
+  plugins: [typescriptPlugin({ typescript })],
+  onwarn: function(warning, warn) {
     // Suppress this error message... there are hundreds of them. Angular team says to ignore it.
     // https://github.com/rollup/rollup/wiki/Troubleshooting#this-is-undefined
     if ("THIS_IS_UNDEFINED" === warning.code) return
     if ("CIRCULAR_DEPENDENCY" === warning.code) return
 
     warn(warning)
-  },
+  }
 }
